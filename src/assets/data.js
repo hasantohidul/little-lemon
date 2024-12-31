a2f252a7236661754147dfa469284aedb1cde687
Mohammad Tohidul Hasan <tohidul9849@gmail.com>
2024-12-31 22:59:14 +0600

    feat: add data.js to organize image data

---
+++ src/assets/data.js
@@ -0,0 +1,27 @@
+import greekSalad from "./greek salad.jpg";
+import bruschetta from "./bruschetta.jpg";
+import lemonDessert from "./lemon dessert.jpg";
+
+export const imageData = [
+    {
+      id: 1,
+      name: "Greek Salad",
+      src: greekSalad,
+      description: "A delicious Greek salad with fresh ingredients.",
+      price: "$12.99",
+    },
+    {
+      id: 2,
+      name: "Bruschetta",
+      src: bruschetta,
+      description: "Crispy bruschetta topped with fresh tomatoes and basil.",
+      price: "$9.99",
+    },
+    {
+      id: 3,
+      name: "Lemon Dessert",
+      src: lemonDessert,
+      description: "A refreshing lemon dessert to complete your meal.",
+      price: "$6.49",
+    },
+  ];

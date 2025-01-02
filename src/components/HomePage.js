e34dd1ffb3784d678f8888486c39d3196677ed44
Mohammad Tohidul Hasan <tohidul9849@gmail.com>
2025-01-02 23:20:10 +0600

    feat: add HomePage

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import Menu from "./Menu";
+import Testimonials from "./Testimonials";
+import About from "./About";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Hero />
+      <Menu />
+      <Testimonials />
+      <About />
+    </div>
+  );
+}
